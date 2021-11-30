9a8130f58eb40471f790f14b8fe15d68dae591d3
shoiraTa <shoira.shakirovna@gmail.com>
2021-11-30 18:03:44 +0500

    add: pages

--- src/componets/CalculatorUI.js
+++ src/componets/CalculatorUI.js
@@ -4,6 +4,7 @@
 import './calculatorUI.css';
 import React, { useState } from 'react';
 import calculate from '../logic/calculate';
+import Header from './Header';
 
 const Calculator = () => {
   const [state, setState] = useState({ total: '0', next: null, operation: null });
@@ -13,36 +14,41 @@ const Calculator = () => {
   };
 
   return (
-    <div className="calculator">
-      <div className="calculator-container">
-        <div className="result">
-          {state.total}
-          {state.operation}
-          {state.next}
-        </div>
-        <div className="calculator-oprtaitions">
-          <button type="button" onClick={handleClick} className="bg-gray single-cell ">AC</button>
-          <button type="button" onClick={handleClick} className="bg-gray single-cell ">%</button>
-          <button type="button" onClick={handleClick} className="bg-gray single-cell ">+/-</button>
-          <button type="button" onClick={handleClick} className="bg-orange single-cell">&divide;</button>
-          <button type="button" onClick={handleClick} className="bg-gray single-cell">7</button>
-          <button type="button" onClick={handleClick} className="bg-gray single-cell ">8</button>
-          <button type="button" onClick={handleClick} className="bg-gray single-cell ">9</button>
-          <button type="button" onClick={handleClick} className="bg-orange single-cell">x</button>
-          <button type="button" onClick={handleClick} className="bg-gray single-cell ">4</button>
-          <button type="button" onClick={handleClick} className="bg-gray single-cell">5</button>
-          <button type="button" onClick={handleClick} className="bg-gray single-cell">6</button>
-          <button type="button" onClick={handleClick} className="bg-orange single-cell">-</button>
-          <button type="button" onClick={handleClick} className="bg-gray single-cell">1</button>
-          <button type="button" onClick={handleClick} className="bg-gray single-cell">2</button>
-          <button type="button" onClick={handleClick} className="bg-gray single-cell">3</button>
-          <button type="button" onClick={handleClick} className="bg-orange single-cell">+</button>
-          <button type="button" onClick={handleClick} className="bg-gray zero single-cell">0</button>
-          <button type="button" onClick={handleClick} className="bg-gray single-cell">.</button>
-          <button type="button" onClick={handleClick} className="bg-orange single-cell">=</button>
+    <>
+      <Header />
+
+      <div className="calculator">
+        <h1>Let&apos;s do some Math</h1>
+        <div className="calculator-container">
+          <div className="result">
+            {state.total}
+            {state.operation}
+            {state.next}
+          </div>
+          <div className="calculator-oprtaitions">
+            <button type="button" onClick={handleClick} className="bg-gray single-cell ">AC</button>
+            <button type="button" onClick={handleClick} className="bg-gray single-cell ">%</button>
+            <button type="button" onClick={handleClick} className="bg-gray single-cell ">+/-</button>
+            <button type="button" onClick={handleClick} className="bg-orange single-cell">&divide;</button>
+            <button type="button" onClick={handleClick} className="bg-gray single-cell">7</button>
+            <button type="button" onClick={handleClick} className="bg-gray single-cell ">8</button>
+            <button type="button" onClick={handleClick} className="bg-gray single-cell ">9</button>
+            <button type="button" onClick={handleClick} className="bg-orange single-cell">x</button>
+            <button type="button" onClick={handleClick} className="bg-gray single-cell ">4</button>
+            <button type="button" onClick={handleClick} className="bg-gray single-cell">5</button>
+            <button type="button" onClick={handleClick} className="bg-gray single-cell">6</button>
+            <button type="button" onClick={handleClick} className="bg-orange single-cell">-</button>
+            <button type="button" onClick={handleClick} className="bg-gray single-cell">1</button>
+            <button type="button" onClick={handleClick} className="bg-gray single-cell">2</button>
+            <button type="button" onClick={handleClick} className="bg-gray single-cell">3</button>
+            <button type="button" onClick={handleClick} className="bg-orange single-cell">+</button>
+            <button type="button" onClick={handleClick} className="bg-gray zero single-cell">0</button>
+            <button type="button" onClick={handleClick} className="bg-gray single-cell">.</button>
+            <button type="button" onClick={handleClick} className="bg-orange single-cell">=</button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
+import Home from './pages/Home';
+import Quotes from './pages/Quotes';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <Routes>
+      <Route path="Calculator/" element={<Home />} />
+      <Route path="Calculator/app" element={<App />} />
+      <Route path="Calculator/quotes" element={<Quotes />} />
+    </Routes>
+  </Router>,
+  document.getElementById('root'),
+);
